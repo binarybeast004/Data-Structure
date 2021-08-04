e6bd73cd3716e1579902689bca0cdfda1229aa30
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-08-04 20:01:15 +0600

    Update insertion.cpp

--- Array/insertion.cpp
+++ Array/insertion.cpp
@@ -1,3 +1,40 @@
+// 10	20	30	40	50	60	70	80	90	
+// N=20
+// Add  77 in index 5
+
+
+#include<iostream>
+using namespace std;
+int main() {
+// insertion
+int n=20;
+int a[n] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+for (int i=0; i<n; i++){
+   cout << a[i] <<" ";
+}
+cout<<"\n";
+int size=9; 
+int position=6;
+int value=77;
+for(int i=size; i>position; i--){
+  
+    a[i]=a[i-1]  ;
+}
+a[position]= value;
+
+for(int i=0;i<n; i++){
+    cout<<a[i]<<" ";
+}
+size++;
+return 0;
+}
+
+
+
+// 10, 20, 30, 40, 50
+//   N=10
+//   add 99 at index 1
+
 #include<iostream>
 using namespace std;
 int main() {
