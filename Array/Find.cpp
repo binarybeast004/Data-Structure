0dce2e1cbac46a65a3052cfe45ad9fc3a2c59a4b
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-08-04 21:14:06 +0600

    Update Find.cpp

--- Array/Find.cpp
+++ Array/Find.cpp
@@ -1,4 +1,45 @@
 
+
+// 10	20	30	40	50	60	70	80	90	
+// N=20
+// Find 70 ?
+
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+// linear search
+int n=20;
+int a[n]={10, 20, 30,40,50,60,70,80,90};
+int size=9;
+int find=70;
+int  index=-1;
+for(int i=0; i<size;i++){
+    if(a[i]==find){
+        index=i;
+        break;
+    }
+}
+if(index!=-1){
+    cout<<"Element found at index "<<index<<"\n";
+}
+else{
+    cout<<"Element not found";
+}
+return 0;
+}
+
+
+
+
+
+
+// 10, 20, 30, 40, 50
+// n = 10;
+// find 50
+
 #include<iostream>
 using namespace std;
 
